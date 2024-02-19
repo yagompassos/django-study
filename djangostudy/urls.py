16b1fbad76b8dde90b23f880b0a301829423ed25
Yago <yagompassos@gmail.com>
2024-02-19 11:17:45 -0300

    Alterando urls

--- djangostudy/urls.py
+++ djangostudy/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import eventos
 
 urlpatterns = [
+    path('', eventos.urls),
     path('admin/', admin.site.urls),
 ]
